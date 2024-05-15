fae5263007c2bea4f09247d5a20ff56a5649f0ae
zhiyenbetov_51826 <Azamat.Zhiyenbetov@kaspi.kz>
2024-05-15 22:17:41 +0500

    feat: animal tests

---
+++ src/main/java/com/example/Lion.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.List;
+
+public class Lion {
+
+    Feline feline;
+    boolean hasMane;
+
+    public Lion(String sex) throws Exception {
+        if ("Самец".equals(sex)) {
+            hasMane = true;
+        } else if ("Самка".equals(sex)) {
+            hasMane = false;
+        } else {
+            throw new Exception("Используйте допустимые значения пола животного - самец или самка");
+        }
+    }
+
+    public Lion(Feline feline){
+        this.feline = feline;
+    }
+
+    public int getKittens() {
+        return feline.getKittens();
+    }
+
+    public boolean doesHaveMane() {
+        return hasMane;
+    }
+
+    public List<String> getFood() throws Exception {
+        return feline.getFood("Хищник");
+    }
+}
